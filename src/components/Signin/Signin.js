174777df7af213aec526d82ecc6555fdb5f9721e
MENG-KU CHEN <60210975+nick981837@users.noreply.github.com>
2020-08-25 21:30:33 +0800

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 class Signin extends React.Component{
 constructor(props){
   super(props);
@@ -17,7 +16,7 @@ onEmailChange = (event) =>{
 onPasswordChange = (event) =>{
   this.setState({signInPassword :event.target.value})
 }
-
+//Send user's email and password to back-end, and check if they're match
 onSubmitSignin = ()=>{
   fetch(' https://damp-sea-30110.herokuapp.com/signin', {
     method: 'post', 
@@ -34,12 +33,12 @@ onSubmitSignin = ()=>{
        this.props.onRouteChange('home');
     } 
    })
-   
 }
 
 render(){
   const {onRouteChange}  = this.props;
   return(
+// Signin Form from tachyons
   <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
   <main className="pa4 black-80">
   <div className="measure ">
@@ -63,14 +62,13 @@ render(){
     </div>
     <div className="lh-copy mt3">
       <p onClick={()=>onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-     
     </div>
   </div>
 </main>
 </article>
 );   
 }
- 
+
 }
 
 export default Signin;
