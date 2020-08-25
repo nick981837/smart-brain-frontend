a3316a56c177aecf91071f2089d80a3175bc803a
MENG-KU CHEN <60210975+nick981837@users.noreply.github.com>
2020-08-25 21:16:20 +0800

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,22 @@
 import React from 'react'
 
 const Navigation=({onRouteChange, isSignedIn})=>{
-		if (isSignedIn){
+	//If in Home Page, display only 'Sign Out' Navigation
+	if (isSignedIn){
 	return(
-	<nav style={{display:'flex', justifyContent:'flex-end'}}>
-		<p onClick={()=>onRouteChange('signin')} className='f1 link dim black underline pa3 pointer'>Sign Out</p>
-	</nav>
+	  <nav style={{display:'flex', justifyContent:'flex-end'}}>
+	  <p onClick={()=>onRouteChange('signin')} className='f1 link dim black underline pa3 pointer'>Sign Out</p>
+	  </nav>
 	);
+	//If in Signin or Register Page, display 'Signin' and 'Reigster' Navigation
 	}else{
-		return(
-    <nav style={{display:'flex', justifyContent:'flex-end'}}>
-		<p onClick={()=>onRouteChange('signin')} className='f1 link dim black underline pa3 pointer'>Sign In</p>
-	    <p onClick={()=>onRouteChange('register')} className='f1 link dim black underline pa3 pointer'>Register</p>
-	</nav>
-
+	return(
+          <nav style={{display:'flex', justifyContent:'flex-end'}}>
+	  <p onClick={()=>onRouteChange('signin')} className='f1 link dim black underline pa3 pointer'>Sign In</p>
+	  <p onClick={()=>onRouteChange('register')} className='f1 link dim black underline pa3 pointer'>Register</p>
+	  </nav>
 	)
 	}
-	
 }
+
 export default Navigation;
